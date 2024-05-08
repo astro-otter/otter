56d3f45c2ef0a425af3c1a10a70c16753fe72504
Noah Franz <noahfranz13@gmail.com>
2024-05-08 11:33:16 -0700

    fix bug in summary table generator script

--- scripts/gen_summary_table.py
+++ scripts/gen_summary_table.py
@@ -4,17 +4,17 @@ Script to generate the summary table for OTTER searches
 This needs to be run everytime this repo is downloaded right now because
 the summary table stores absolute paths in it which is user dependent
 '''
-
+import os
 import argparse
 from otter import Otter
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument('--otterdir', help='Directory where the otter json files will go')
+    p.add_argument('--otterroot', help='Directory where the otter json files will go')
     args = p.parse_args()
 
     # find the otter directory and then generate the summary table
-    db = Otter(args.otterdir)
+    db = Otter(os.path.join(args.otterroot, 'otterdb', '.otter'))
     db.generate_summary_table(save=True)
 
 if __name__ == '__main__':
