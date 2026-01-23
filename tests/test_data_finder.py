002d951fb149730345651110302540cb98476bdf
Noah Franz <noahfranz13@gmail.com>
2026-01-23 15:56:20 -0700

    skip taahe DataFinder.query_wisrep metest since it queries wiserep too fast back to back and then fails

--- tests/test_data_finder.py
+++ tests/test_data_finder.py
@@ -270,6 +270,7 @@ def test_query_heasarc():
     assert len(res2) >= 2, "Missing some HEASARC data"
 
 
+@pytest.skip("this test crashes wiserep because too many queries back to back")
 def test_query_wiserep():
     """
     Test querying WISeREP for spectra
